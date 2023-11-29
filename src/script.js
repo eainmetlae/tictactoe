06b4ae887e55d459ef42537bff9970cf02161521
eainmetlae <eainmetlae@gmail.com>
2023-11-29 11:58:33 +0630

    added some UI fix

--- src/script.js
+++ src/script.js
@@ -3,12 +3,8 @@
 console.log("in js");
 
 const containerEl = document.querySelector(".container");
-
 let currentPlayer = "P";
-// const playerMove = [];
-// const compMove = [];
 
-//switch to change
 const playerMove = {
   player: "player",
   dgGroup: [[], []],
@@ -28,11 +24,9 @@ const updatePlayerMove = function (move, whoseMove) {
   const numA = Number.parseInt(a);
   const numB = Number.parseInt(b);
 
-  //if (currentPlayer === "P") {
   if (numA === numB) {
     //0,0 1,1, 2,2
     whoseMove.dgGroup[0].push(move);
-
     if (numA === 1) whoseMove.dgGroup[1].push(move);
   }
   if (numA + numB === 2 && numA !== 1) {
@@ -40,8 +34,6 @@ const updatePlayerMove = function (move, whoseMove) {
   }
   whoseMove.vtGroup[a].push(move);
   whoseMove.hrGroup[b].push(move);
-  console.log(playerMove);
-  console.log(compMove);
 };
 
 const countMoves = function () {
@@ -50,14 +42,7 @@ const countMoves = function () {
   return { C: circleElements?.length, P: crossElements?.length };
 };
 
-const checkWinner = function () {
-  //possible outcomes: winner, '', 'draw'
-  const { C: computer, P: player } = countMoves();
-  const totalMoves = computer + player;
-
-  const maxMove = Math.max(computer, player);
-  if (maxMove < 3) return "";
-
+const getMoves = function () {
   let finalResult;
   if (currentPlayer === "P")
     finalResult = [
@@ -71,6 +56,29 @@ const checkWinner = function () {
       ...compMove.hrGroup,
       ...compMove.vtGroup,
     ];
+  return finalResult;
+};
+
+const updateWinningTrack = function () {
+  const finalResult = getMoves();
+  const winningPath = finalResult.find((item) => item.length === 3);
+  console.log(winningPath);
+  winningPath.forEach((item) => {
+    const el = document.querySelector(`[data-val="${item}"]`);
+    el.style.backgroundColor = "#8ce99a";
+  });
+};
+
+const checkWinner = function () {
+  //possible outcomes: winner, '', 'draw'
+  const { C: computer, P: player } = countMoves();
+  const totalMoves = computer + player;
+
+  const maxMove = Math.max(computer, player);
+  if (maxMove < 3) return "";
+
+  const finalResult = getMoves();
+
   console.log(finalResult);
   const winningMove = finalResult.some((item) => item.length === 3);
   if (winningMove) return currentPlayer;
@@ -81,21 +89,32 @@ const checkWinner = function () {
 
 containerEl.addEventListener("click", function (e) {
   const clickedEl = e.target.closest(".square");
+  // 1) check if the cell is already occupied
   if (clickedEl.children.length > 0) return;
 
+  // 2) Mark cell
   const moveMarkup = `<div class="${
     currentPlayer === "P" ? "cross-big" : "circle-big"
   }"></div>`;
   clickedEl.insertAdjacentHTML("afterbegin", moveMarkup);
+
+  // 3) Update player move
   if (currentPlayer === "P")
     updatePlayerMove(clickedEl.dataset.val, playerMove);
   else updatePlayerMove(clickedEl.dataset.val, compMove);
+
+  // 4) For every move, check if there is winner.
   const winner = checkWinner();
   if (winner === "")
-    //switch player
     currentPlayer === "P" ? (currentPlayer = "C") : (currentPlayer = "P");
   else {
-    alert(`${winner} won!!!`);
-    location.reload();
+    if (winner === "draw") alert("It's a draw!");
+    else {
+      updateWinningTrack();
+      alert(`${winner} won!!`);
+    }
+    setTimeout(() => {
+      location.reload();
+    }, 5000);
   }
 });
